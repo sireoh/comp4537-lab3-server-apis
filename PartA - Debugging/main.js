be9ae7b26bada91d857c3887fb26b72eff2312e1
Vincent <eohndk@gmail.com>
2025-09-23 22:37:24 -0700

    completed part A

---
+++ PartA - Debugging/main.js
@@ -0,0 +1,5 @@
+const math = require("./modules/math.js");
+
+console. log(`Hello VINCENT. The sum is ${math. add(219, 1024)}`) ;
+
+console. log(`Hello VINCENT. The difference is ${math. subtract(1024, 219)}`) ;
